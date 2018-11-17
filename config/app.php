ef8177f706e18867d810b3bf0ce1015b979b5be8
ThangDang <tkpproo@gmail.com>
2018-11-17 23:26:24 +0700

    fix conflict

--- config/app.php
+++ config/app.php
@@ -159,8 +159,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
-
         /*
          * Package Service Providers...
          */
@@ -222,7 +222,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
